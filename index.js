ee5b7cf903858fa96fb42b171b4a3b858446ab70
xyaiwh <2356737196@qq.com>
2020-09-29 00:43:57 +0800

    first

---
+++ index.js
@@ -0,0 +1,56 @@
+// const request= require('request')
+const http = require('http')
+const fs = require('fs');
+const cheerio = require('cheerio');
+const iconv = require('iconv-lite');
+// const superAgent = require('superagent');
+
+
+const url='http://book.zongheng.com/chapter/685640/38883752.html';
+let page=1,
+    lastPage=30;
+
+function downLoad(url) {
+   http.get(url,(res)=>{
+    //    console.log(res);
+       var html='';
+       res.on('data',(chunk)=>{
+        //    console.log(chuck);
+           html+=chunk.toString('utf-8')
+       })
+       res.on('end',()=>{
+        //    //把获取到的buffer数据转码成gbk格式
+        // const html1 = iconv.decode(Buffer.concat(html), 'GBK');
+        const $ = cheerio.load(html, {decodeEntities: false});
+        // console.log($,html1);
+        const title=$('.title .title_txtbox').text();
+        const bookName=$('.reader_crumb a').eq(2).text();
+        let content=$('.content').html().split('</p><p>');
+         content[0]=content[0].split('<p>')[1];
+         content[content.length-1]=content[content.length-1].split('</p>')[0];
+        console.log(title,bookName);
+        let bookObj={
+            id:page,
+            err:0,
+            bookName:bookName,
+            title:title,
+            content:content
+        };
+        saveContent(bookObj);
+        // console.log(html1);
+       })
+   })
+        
+}
+function saveContent(obj) {
+    console.log(`${page}--${obj.title}`)
+     //判断书名文件夹是否存在，不存在则创建
+    if (!fs.existsSync(`data/${obj.bookName}`)) {
+         fs.mkdirSync(`data/${obj.bookName}`)
+     }
+    //写入json文件
+     fs.writeFile(`./data/${obj.bookName}/chapter${page}.json`, JSON.stringify(obj), 'utf-8', err => {
+        if (err) throw err
+     })
+ }
+downLoad(url);
